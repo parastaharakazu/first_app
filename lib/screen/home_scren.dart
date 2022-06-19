4329053bc6c74e0675edea4ef09af126b8a5a8bb
parastaharakazu <harakazu@parallelstage.com>
2022-06-19 10:47:45 +0900

    step 2

--- lib/screen/home_scren.dart
+++ lib/screen/home_scren.dart
@@ -7,9 +7,18 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First App'),
+        title: Text('First App (Updated)'), // (1)
       ),
-      body: Text('最初のアプリです'),
+      body: Center(
+        // (6)
+        child: Text(
+          'テキストを変更してみました', // (3)
+          style: TextStyle(
+            color: Colors.purple, // (4),
+            fontSize: 50, // (5)
+          ),
+        ),
+      ),
     );
   }
 }
